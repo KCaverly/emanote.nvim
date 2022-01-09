c87492a6510001603196eee806a0df22d16a53f2
KCaverly <kylebcaverly@gmail.com>
2022-01-08 22:38:19 -0500

    progress

--- lua/emanote/emanote.lua
+++ lua/emanote/emanote.lua
@@ -1,11 +1,30 @@
+local Path = require("plenary/path")
+local uv = vim.loop
 
 local M = {}
 
 function M.sayHelloWorld() print('Hello world!') end
 
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
 -- Function to create new note
 local function createNewNote(name)
-  print(name)
+
+  config_path = "~/personal/kb"
+  exec = ":e " .. config_path .. "/" .. name
+  vim.api.nvim_exec(exec, 0)
+
 end
 
 function M.newNote() 
